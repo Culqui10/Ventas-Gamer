ca706316b4bf27110f8a11a1027d9988de7a89c9
crystian <crys100culqui@gmail.com>
2024-09-22 15:22:14 -0500

    cambios imagenes

--- js/script.js
+++ js/script.js
@@ -4,28 +4,6 @@ function agregarCarrito(producto) {
     window.location.href = "carrito.html?producto=" + encodeURIComponent(producto);
 }
 
-
-function cargaarCategoria(categorias) {
-    // Ruta del archivo de la categoría
-    const ruta = `categorias/${categorias}.html`;
-    console.log("Ruta:", ruta); // Depuración para verificar la ruta
-    // Obtener el contenedor de productos
-    const productosDiv = document.getElementById('productos');
-    // Cargar el archivo HTML de la categoría
-    fetch(ruta)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Error al cargar el archivo: " + response.statusText);
-            }
-            return response.text();
-        })
-        .then(data => {
-            productosDiv.innerHTML = data;
-        })
-        .catch(error => {
-            console.error('Error al cargar la categoría:', error);
-        });
-}
 function cargarCategoria(categoria) {
     // Evitamos la carga del inicio porque ya está en el HTML principal
     if (categoria === 'Inicio') return;
@@ -48,4 +26,5 @@ function cargarCategoria(categoria) {
             console.error('Error al cargar la categoría:', error);
         });
 }
+
 
